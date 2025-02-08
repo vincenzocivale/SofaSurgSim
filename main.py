beca5fbb85394eaa0fda91814a20bdb310323f8a
Yuto17 <civalevincenzoyuto@gmail.com>
2025-02-08 15:52:17 +0100

    refactor:  SOFASceneController to support GUI initialization and update OrganManager to remove ROS topic subscription

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from ros_interface import ROSInterface
 from sofa_scene_controller import SOFASceneController
 
 def main():
-    ros_interface = ROSInterface()
-    sofa_scene_controller = SOFASceneController(ros_interface)
+    #ros_interface = ROSInterface()
+    sofa_scene_controller = SOFASceneController(None, True)
     sofa_scene_controller.run_simulation()
     
+if __name__ == "__main__":
+    main()
